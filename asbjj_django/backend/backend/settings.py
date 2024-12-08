61802aabd22725bb6cb6466ab345aaf8d575c230
fabianosf <32664815+fabianosf@users.noreply.github.com>
2024-12-08 15:01:05 -0300

    Update settings.py

--- asbjj_django/backend/backend/settings.py
+++ asbjj_django/backend/backend/settings.py
@@ -154,4 +154,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fabiano.freitas@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fab260281xx'
+EMAIL_HOST_PASSWORD = 'senha'
